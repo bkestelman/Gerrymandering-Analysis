811f56eae0b0b2a9e6c66a09748df8289a5d89f9
JohnsonLu3 <JohnsonLu94@gmail.com>
2017-10-14 19:57:20 -0400

    started work on Demo for the database

---
+++ DataBaseDemo/src/main/java/demo/main.java
@@ -0,0 +1,13 @@
+package demo;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan
+public class main{
+
+
+  public static void main(String[] args) {
+
+  }
+}
