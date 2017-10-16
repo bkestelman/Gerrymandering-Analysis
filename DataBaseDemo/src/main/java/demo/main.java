f0bc6f3150165fe4e454290aa9ca0442727878cf
JohnsonLu3 <JohnsonLu94@gmail.com>
2017-10-16 13:38:15 -0400

    Added application.properties to connect to database

--- DataBaseDemo/src/main/java/demo/main.java
+++ DataBaseDemo/src/main/java/demo/main.java
@@ -1,10 +1,37 @@
 package demo;
 
+import Entity.State;
+import Repository.DistrictRepository;
+import Repository.StateRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Entity.District;
+
 
 public class main{
 
+	public static StateRepository SR;
+	public static DistrictRepository DR;
 
-  public static void main(String[] args) {
-
+	public static void main(String[] args) {
+		// Create new state and district
+		State newState = new State();
+		District newDistrict = new District();
+	  
+		// fill the tables with data
+		
+		
+		// Save entities to database
+		SR.save(newState);
+		DR.save(newDistrict);
+	  
+		// Pull the data from the database
+		Iterable<State> states = SR.findAll();
+		Iterable<District> districts = DR.findAll();
+		
+		// print out the data
+		
   }
 }
